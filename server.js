5d2db2123e10122ec26380852252670b83b1e5f6
Nikos <nikos.cristodoulias@gmail.com>
2026-04-25 01:35:58 +0300

    Add server startup log

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from "express";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (_, res) => res.send("Live"));
 
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
